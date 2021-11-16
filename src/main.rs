f4506b96f48d51a37ef4a4012e522d21bc5d2a21
Jack Baron <jackmbaron@gmail.com>
2021-11-16 19:22:13 +0000

    make subcommand

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,37 @@ use clap::Parser;
 use color_eyre::Result;
 
 #[derive(Debug, Parser)]
-struct Args {
-    /// Server port to start counting at
-    #[clap(short = 'p', long, default_value = "25565")]
-    start_port: u16,
+enum Args {
+    // Initialise test servers
+    Init {
+        /// Server port to start counting at
+        #[clap(short = 'p', long, default_value = "25565")]
+        start_port: u16,
 
-    /// World seed for all servers
-    #[clap(short, long)]
-    level_seed: String,
+        /// World seed for all servers
+        #[clap(short, long)]
+        level_seed: String,
 
-    /// MOTD Template, prepends server index
-    #[clap(short, long, default_value = "Mammoth Server")]
-    motd_template: String,
+        /// MOTD Template, prepends server index
+        #[clap(short, long, default_value = "Mammoth Server")]
+        motd_template: String,
 
-    /// Don't copy Plugins directory
-    #[clap(short, long)]
-    skip_plugins: bool,
+        /// Don't copy Plugins directory
+        #[clap(short, long)]
+        skip_plugins: bool,
 
-    /// Don't copy bukkit.yml
-    #[clap(long)]
-    no_copy_bukkit: bool,
+        /// Don't copy bukkit.yml
+        #[clap(long)]
+        no_copy_bukkit: bool,
 
-    /// Don't copy spigot.yml
-    #[clap(long)]
-    no_copy_spigot: bool,
+        /// Don't copy spigot.yml
+        #[clap(long)]
+        no_copy_spigot: bool,
 
-    /// Don't copy paper.yml
-    #[clap(long)]
-    no_copy_paper: bool,
+        /// Don't copy paper.yml
+        #[clap(long)]
+        no_copy_paper: bool,
+    }
 }
 
 fn main() -> Result<()> {
