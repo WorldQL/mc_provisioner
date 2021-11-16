e62358776d7032ff8c53441afbb859a5a02a50d8
Jack Baron <jackmbaron@gmail.com>
2021-11-16 22:40:37 +0000

    apply clippy lint

--- src/utils.rs
+++ src/utils.rs
@@ -5,7 +5,7 @@ type ServerInfo = (u8, u16, PathBuf, String);
 pub fn server_iter(
     server_count: u8,
     start_port: u16,
-    directory_template: &String,
+    directory_template: &str,
 ) -> impl Iterator<Item = ServerInfo> + '_ {
     (1..=server_count).into_iter().map(move |idx| {
         let port = start_port + (idx as u16 - 1);
