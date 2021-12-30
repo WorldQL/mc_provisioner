560ef4b735ecb0437dae13fd6ced2ebf9bcd7481
Jack Baron <jackmbaron@gmail.com>
2021-12-30 07:12:09 +0000

    run and restart with specified args closes #3

--- src/cmd_start_stop.rs
+++ src/cmd_start_stop.rs
@@ -3,7 +3,52 @@ use color_eyre::Result;
 use tracing::{error, info};
 
 use crate::config::{GlobalArgs, StartArgs};
-use crate::utils;
+use crate::utils::{self, ServerMemory};
+
+fn generate_jvm_args(args: StartArgs) -> String {
+    let mut flags = vec![];
+    flags.push(format!("-Xmx{}", args.max_memory));
+
+    if args.use_aikar_flags {
+        let large_mem = args.max_memory >= ServerMemory::from("12G");
+
+        let new_size_percent = if large_mem { 40 } else { 30 };
+        let max_new_size_percent = if large_mem { 50 } else { 40 };
+        let heap_region_size = if large_mem { "16M" } else { "8M" };
+        let reserve_percent = if large_mem { 15 } else { 20 };
+        let init_heap_occupancy_percent = if large_mem { 20 } else { 15 };
+
+        flags.push("-XX:+UseG1GC".into());
+        flags.push("-XX:+ParallelRefProcEnabled".into());
+        flags.push("-XX:MaxGCPauseMillis=200".into());
+        flags.push("-XX:+UnlockExperimentalVMOptions".into());
+        flags.push("-XX:+DisableExplicitGC".into());
+        flags.push("-XX:+AlwaysPreTouch".into());
+        flags.push(format!("-XX:G1NewSizePercent={}", new_size_percent));
+        flags.push(format!("-XX:G1MaxNewSizePercent={}", max_new_size_percent));
+        flags.push(format!("-XX:G1HeapRegionSize={}", heap_region_size));
+        flags.push(format!("-XX:G1ReservePercent={}", reserve_percent));
+        flags.push("-XX:G1HeapWastePercent=5".into());
+        flags.push("-XX:G1MixedGCCountTarget=4".into());
+        flags.push(format!(
+            "-XX:InitiatingHeapOccupancyPercent={}",
+            init_heap_occupancy_percent
+        ));
+        flags.push("-XX:G1MixedGCLiveThresholdPercent=90".into());
+        flags.push("-XX:G1RSetUpdatingPauseTimePercent=5".into());
+        flags.push("-XX:SurvivorRatio=32".into());
+        flags.push("-XX:+PerfDisableSharedMem".into());
+        flags.push("-XX:MaxTenuringThreshold=1".into());
+        flags.push("-Dusing.aikars.flags=https://mcflags.emc.gs".into());
+        flags.push("-Daikars.new.flags=true".into());
+    }
+
+    if let Some(jvm_args) = args.jvm_args {
+        flags.push(jvm_args)
+    }
+
+    flags.join(" ")
+}
 
 pub fn start(global_args: GlobalArgs, args: StartArgs) -> Result<()> {
     let server_iter = utils::server_iter(
@@ -12,6 +57,7 @@ pub fn start(global_args: GlobalArgs, args: StartArgs) -> Result<()> {
         &global_args.directory_template,
     );
 
+    let jvm_args = generate_jvm_args(args);
     for (_, _, directory, _) in server_iter {
         let name = directory.to_str().unwrap();
         info!("starting tmux session: {}", &name);
@@ -28,8 +74,8 @@ pub fn start(global_args: GlobalArgs, args: StartArgs) -> Result<()> {
         }
 
         let run = format!(
-            "java -Xmx{} -jar paper.jar nogui ; tmux wait -S {}_exit",
-            &args.max_memory, &name
+            "java {} -jar paper.jar nogui ; tmux wait -S {}_exit",
+            &jvm_args, &name
         );
 
         if run_cmd!(tmux send -t $name $run ENTER).is_err() {
@@ -82,6 +128,7 @@ pub fn restart(global_args: GlobalArgs, args: StartArgs) -> Result<()> {
         &global_args.directory_template,
     );
 
+    let jvm_args = generate_jvm_args(args);
     for (_, _, directory, _) in server_iter {
         let name = directory.to_str().unwrap();
         info!("restarting tmux session: {}", &name);
@@ -98,7 +145,7 @@ pub fn restart(global_args: GlobalArgs, args: StartArgs) -> Result<()> {
             continue;
         }
 
-        let run = format!("java -Xmx{} -jar paper.jar nogui", &args.max_memory);
+        let run = format!("java {} -jar paper.jar nogui", &jvm_args);
         if run_cmd!(tmux send -t $name $run ENTER).is_err() {
             error!("failed to restart \"{}\"", &name);
             continue;
